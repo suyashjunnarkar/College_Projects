c0f45e1a9ac6343bbdce40c3a6094dcaae716bc2
suyashjunnarkar <smjunnarkar_b15@el.vjti.ac.in>
2018-04-11 23:59:58 +0530

    Readme commit

--- RandomForrestClassifier/Codes/RandomForrestClassifier.py
+++ RandomForrestClassifier/Codes/RandomForrestClassifier.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error, r2_score,accuracy_score
 from treeinterpreter import treeinterpreter as ti
+from sklearn.tree import export_graphviz
 import graphviz
+import warnings
+
+warnings.filterwarnings("ignore")
 
 # Load mobile data data.
 data = pd.read_csv('../data/train.csv',index_col=0)
@@ -14,7 +19,7 @@ X = data.drop('output', axis=1)
 
 #Model training
 model = RandomForestClassifier(n_estimators = 1000, oob_score = True, n_jobs = -1,random_state =50,max_features = "auto", min_samples_leaf = 2)
-model.fit(X,y)
+finalTree = model.fit(X,y)
 
 #taking test data
 testData = pd.read_csv('../data/test.csv',index_col=0)
@@ -27,3 +32,16 @@ pred = model.predict(X_test)
 #printing evaluating parameters
 print ("MSE = ",mean_squared_error(y_test, pred))
 print ("Accuracy = ",accuracy_score(y_test, pred))
+
+print("1000 trees are iterating!!! ")
+ 
+for tree_in_forest in finalTree.estimators_:
+	export_graphviz(tree_in_forest,feature_names=X.columns,filled=True,rounded=True)
+   
+print ("Generating png file ...")
+os.system('dot -Tpng tree.dot -o tree.png')
+with open("tree.dot") as f:
+    dot_graph=f.read()
+graphviz.Source(dot_graph)
+
+print("Open Codes folder to see the tree.png file")
---
+++ RandomForrestClassifier/Readme.txt
@@ -0,0 +1,19 @@
+You need the following packages (For python3) - 
+numpy - pip3 install numpy
+pandas - pip3 install pandas
+sklearn - pip3 install sklearn
+graphviz - pip3 install graphviz
+
+You need to install graphviz in your computer's default path too-
+
+For MacOS - brew install graphviz
+
+For Ubuntu -  sudo apt-get install graphviz
+
+For Windows-
+Install windows package from: http://www.graphviz.org/Download_windows.php
+Install python graphviz package
+Add C:\Program Files (x86)\Graphviz2.38\bin to User path
+Add C:\Program Files (x86)\Graphviz2.38\bin\dot.exe to System Path
+
+If you don't want to see the decision tree comment last section
